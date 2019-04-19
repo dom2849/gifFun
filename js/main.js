9daaa81542fe8847be10f5e839f9a18a272dac5f
Domenico Manna <mannadomenico2849@gmail.com>
2019-04-19 19:50:24 -0400

    Add ability to search for gifs

--- js/main.js
+++ js/main.js
@@ -3,26 +3,55 @@ import UserInterfaceHelper from './userInterfaceHelper.js';
 
 const giphyClient = new GiphyClient();
 const uiHelper = new UserInterfaceHelper();
-const myApp = { offset : 0 };
+
+const myApp = { 
+    offset : 0,
+    currentUrl : giphyClient.trendingUrl
+};
 
 loadEventListeners()
 
 function loadEventListeners() {
     window.addEventListener('scroll', handleBottomOfThePageScroll);
-    window.addEventListener('load', handleTrendingGifsDisplay);
+    window.addEventListener('load', displayTrendingGifs);
+    document.querySelector(".main-navigation__search-icon").addEventListener('click', handleGifSearch);
+    document.querySelector(".main-navigation__search").addEventListener('keyup', handleKeyPress);
+    document.querySelector(".main-navigation__home-button").addEventListener('click', displayTrendingGifs);
 }
 
-function handleBottomOfThePageScroll() {
-    if ((window.innerHeight + window.scrollY + 150) >= document.body.offsetHeight) {
-        myApp.offset += 25
-        handleTrendingGifsDisplay(myApp.offset)
-    }
+function handleKeyPress(event){
+    if (event.code == 'Enter') handleGifSearch();
 }
 
-function handleTrendingGifsDisplay(offset) {
-    giphyClient.fetchData(giphyClient.trendingUrl, offset)
+function cleanUp(){
+    uiHelper.clearAllMasonryItems();
+    myApp.offset = 0;
+    uiHelper.addMessageContent("");
+}
+
+function handleGifSearch(){
+    cleanUp();
+    
+    let userSearch = document.querySelector(".main-navigation__search-input");
+    myApp.currentUrl = giphyClient.getSearchForUrl(userSearch.value);
+
+    displayGifs(myApp.currentUrl, 0);
+    uiHelper.setGifsTitle(userSearch.value);
+    userSearch.value = ''
+}
+
+function displayTrendingGifs(){
+    cleanUp();
+    myApp.currentUrl = giphyClient.trendingUrl;
+    displayGifs(myApp.currentUrl, 0);
+    uiHelper.setGifsTitle("Trending")
+}
+
+function displayGifs(url, offset) {
+    giphyClient.fetchData(url, offset)
         .then((gifs) => {
             addGifs(gifs.data);
+            if (gifs.data.length === 0) uiHelper.addMessageContent('No gifs found');
         })
         .catch((error) => {
             console.log(error);
@@ -34,6 +63,14 @@ function addGifs(gifs) {
         let imageUrl = element.images.original.url;
         uiHelper.appendMasonryItem(imageUrl)
     })
+}
+
+
+function handleBottomOfThePageScroll() {
+    if ((window.innerHeight + window.scrollY + 150) >= document.body.offsetHeight) {
+        myApp.offset += 25
+        displayGifs(myApp.currentUrl, myApp.offset)
+    }
 }
 
 
--- js/userInterfaceHelper.js
+++ js/userInterfaceHelper.js
@@ -1,31 +1,46 @@
-export default function UserInterfaceHelper(){
+export default function UserInterfaceHelper() {
 
 }
 
-
-UserInterfaceHelper.prototype.appendMasonryItem = function(imageUrl){
+UserInterfaceHelper.prototype.appendMasonryItem = function (imageUrl) {
     let masonryGrid = document.querySelector(".gifs__masonry-grid");
     let gridItem = createGridItem(imageUrl);
     masonryGrid.appendChild(gridItem);
 }
 
-function createGridItem(imageUrl){
+function createGridItem(imageUrl) {
     let gridItem = document.createElement("div");
     let gridImage = document.createElement("img");
-    
+
     gridItem.classList = "gifs__grid-item";
     gridImage.src = imageUrl;
     gridImage.classList = "gifs__image"
-    
+
     gridItem.appendChild(gridImage);
-    
-    return gridItem;   
+
+    return gridItem;
 }
 
-UserInterfaceHelper.prototype.clearAllMasonryItems = function(){
+UserInterfaceHelper.prototype.clearAllMasonryItems = function () {
     let masonryGrid = document.querySelector(".gifs__masonry-grid");
 
-    while (masonryGrid.firstChild){
+    while (masonryGrid.firstChild) {
         masonryGrid.removeChild(masonryGrid.firstChild)
     }
+}
+
+UserInterfaceHelper.prototype.setGifsTitle = function (title) {
+    let gifsTitle = document.querySelector(".gifs__title");
+    gifsTitle.textContent = title;
+}
+
+UserInterfaceHelper.prototype.addMessageContent = function (content) {
+    let message = document.querySelector(".gifs__search-message");
+    message.textContent = content
+}
+
+UserInterfaceHelper.prototype.removeMessage = function (className) {
+    let message = document.querySelector(className);
+
+    message.remove();
 }
